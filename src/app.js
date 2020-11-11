a9cc03ec53b878908994364ef471c85168daed3e
diaslilian <lilian.dias25@gmail.com>
2020-11-10 21:06:34 -0300

    import mongoose

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/baltastore", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Successfully made connection.");
+});
+
 const index = require("./routes/index");
-const product = require("./routes/product");
+const product = require("./routes/productRoute");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
